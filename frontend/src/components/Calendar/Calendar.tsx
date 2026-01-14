388628ce002f22f0d6d6ae4c8aa50c39edb0c711
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-01-14 15:17:07 -0300

    organizing css

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './Calendar.css';
+import '../../css/Calendar.css';
 
 const CalendarComponent = () => {
   const [date, setDate] = useState<Date | null>(new Date());
